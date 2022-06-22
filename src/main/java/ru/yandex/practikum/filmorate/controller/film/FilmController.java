62f346e59bbb26643a0b74dcb0abe876ceb799e4
Kirill <kirill@kormilcev.ru>
2022-06-22 23:34:58 +0300

    feat: логирование fix: style: test: docs:

--- src/main/java/ru/yandex/practikum/filmorate/controller/film/FilmController.java
+++ src/main/java/ru/yandex/practikum/filmorate/controller/film/FilmController.java
@@ -27,6 +27,8 @@ public class FilmController {
         if (filmValidation(film)) {
             film.setId(FilmId.getId());
             films.put(film.getId(), film);
+            log.info("Получен POST запрос к эндпоинту: /films, успешно обработан.\n" +
+                            "В коллекцию добавлен фильм: '{}' с id: '{}'." , film.getName(), film.getId());
         }
         return film;
     }
@@ -35,6 +37,8 @@ public class FilmController {
     public Film updateFilm(@RequestBody Film film) {
         if (filmValidation(film)) {
             films.put(film.getId(), film);
+            log.info("Получен PUT запрос к эндпоинту: /films, успешно обработан.\n" +
+                    "В коллекции обновлен фильм: '{}' с id: '{}'." , film.getName(), film.getId());
         }
         return film;
     }
@@ -42,16 +46,24 @@ public class FilmController {
     /** проверка полей добавляемого/обновляемого фильма */
     private boolean filmValidation(Film film) {
         if (film.getDescription().length() > 200) {
+            log.warn("Попытка добавить или обновить фильм: '{}' с описанием более 200 символов:\n'{}'.",
+                    film.getName(), film.getDescription());
             throw new FilmValidationException("Описание фильма содержит более 200 символов.");
         }
         if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            log.warn("Попытка добавить или обновить фильм: '{}' с датой релиза ранее 1895.12.28: '{}'.",
+                    film.getName(), film.getReleaseDate());
             throw new FilmValidationException("Дата релиза фильма ранее 28 декабря 1895 года.");
         }
         if (film.getDuration() < 0) {
+            log.warn("Попытка добавить или обновить фильм: '{}' с отрицательной длительностью: '{}'.",
+                    film.getName(), film.getDuration());
             throw new FilmValidationException("Продолжительность фильма отрицательная.");
         }
         if (film.getId() != 0) {
             if (!films.containsKey(film.getId())) {
+                log.warn("Попытка обновить фильм: '{}' с индексом '{}', отсутствующим в базе.", film.getName(),
+                        film.getId());
                 throw new FilmValidationException("Фильма с таким индексом нет в базе.");
             }
         }
@@ -60,6 +72,8 @@ public class FilmController {
                 if (filmAvailable.getId() == film.getId()) {
                     return true;
                 } else {
+                    log.warn("Попытка обновить или добавить фильм: '{}', который уже есть в базе (id: '{}').",
+                            film.getName(), filmAvailable.getId());
                     throw new FilmValidationException("Фильм с таким названием уже есть в базе.");
                 }
             }
--- src/main/java/ru/yandex/practikum/filmorate/controller/user/UserController.java
+++ src/main/java/ru/yandex/practikum/filmorate/controller/user/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practikum.filmorate.controller.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practikum.filmorate.model.user.User;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     private final HashMap<Integer, User> users = new HashMap<>();
@@ -25,6 +27,8 @@ public class UserController {
         if (userValidation(user)) {
             user.setId(UserId.getId());
             users.put(user.getId(), user);
+            log.info("Получен POST запрос к эндпоинту: /users, успешно обработан.\n" +
+                    "В базу добавлен пользователь: '{}' с id: '{}'." , user.getLogin(), user.getId());
         }
         return user;
     }
@@ -33,6 +37,8 @@ public class UserController {
     public User updateUser(@RequestBody User user) {
         if (userValidation(user)) {
             users.put(user.getId(), user);
+            log.info("Получен PUT запрос к эндпоинту: /users, успешно обработан.\n" +
+                    "В базе обновлен пользователь: '{}' с id: '{}'." , user.getLogin(), user.getId());
         }
         return user;
     }
@@ -40,12 +46,16 @@ public class UserController {
     /** проверка полей добавляемого/обновляемого пользователя */
     private boolean userValidation(User user) {
         if (user.getLogin().contains(" ")) {
+            log.warn("Попытка добавить или обновить пользователя с логином: '{}', содержащего пробелы.", user.getLogin());
             throw new UserValidationException("В логине присутствуют пробелы.");
         }
         if (user.getName().trim().isBlank()) {
+            log.info("Пользователю с логином: '{}' назначено аналогичное имя.", user.getLogin());
             user.setName(user.getLogin());
         }
         if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Попытка добавить или обновить пользователя с логином: '{}' с некорректной датой рождения: '{}'.",
+                    user.getLogin(), user.getBirthday());
             throw new UserValidationException("Не корректная дата рождения.");
         }
         for (User userAvailable : users.values()) {
@@ -53,7 +63,9 @@ public class UserController {
                 if (userAvailable.getId() == user.getId()) {
                     return true;
                 } else {
-                    throw new UserValidationException("Пользователь с такой почтой названием уже зарегистрирован в базе.");
+                    log.warn("Попытка добавить или обновить пользователя с логином: '{}' с уже существующей в базе почтой: '{}'.",
+                            user.getLogin(), user.getEmail());
+                    throw new UserValidationException("Пользователь с такой почтой уже зарегистрирован в базе.");
                 }
             }
         }
